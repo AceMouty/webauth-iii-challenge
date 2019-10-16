18ea2059cae606bc41d2abd2b3738ccd3207113c
Kyle Guerrero <kyleguerrero85@gmail.com>
2019-10-16 15:55:00 -0500

    Created a basic auth express route

--- server.js
+++ server.js
@@ -2,10 +2,16 @@ const express = require('express');
 const server = express();
 const helmet = require('helmet');
 const cors = require('cors');
+// Express Routes
+const authRoutes = require('./routes/auth');
 
 // global middleware
+server.use(cors())
 server.use(helmet())
 server.use(express.json())
+
+// Express Routes
+server.use("/api/auth", authRoutes);
 
 server.get('/', (req, res) => {
   res.status(200).json({message: "It's working"})
